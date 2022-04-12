5be2ea882fa81be97a751c780095f35f023fd646
Mike Dvorscak <mike.dvorscak@gmail.com>
2022-04-11 21:43:12 -0500

    Remove unneeded wasm code

--- www/src/main.js
+++ www/src/main.js
@@ -1,14 +1,6 @@
 import App from './App.svelte';
 import wasm from '../../Cargo.toml';
-
-async function loadWasm() {
-  // const wasm = await import("../../Cargo.toml");
-  // const exports = await wasm.default();
-  const exports = await wasm();
-
-  return exports;
-}
-const exports = await loadWasm();
+const exports = await wasm();
 
 const app = new App({
   target: document.body,
