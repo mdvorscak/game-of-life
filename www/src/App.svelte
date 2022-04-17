50d9e29a1fe31e1c54dedc921d2fab54b14b0401
Mike Dvorscak <mike.dvorscak@gmail.com>
2022-04-16 21:26:17 -0500

    Add link to Github repo

--- www/src/App.svelte
+++ www/src/App.svelte
@@ -2,17 +2,23 @@
   export let wasm;
   import Universe from './Universe.svelte';
   import UniverseControls from './controls/UniverseControls.svelte';
+  import CodeLink from './CodeLink.svelte';
   const UniverseObj = wasm.Universe;
   const memory = wasm.get_memory();
   let universe;
 </script>
 
+<CodeLink />
 <main>
+  <h1>Conway's Game of Life</h1>
   <UniverseControls {universe} />
   <Universe bind:this={universe} {UniverseObj} {memory} />
 </main>
 
 <style>
+  h1 {
+    margin: 0;
+  }
   main {
     position: absolute;
     top: 0;
